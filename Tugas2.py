6804b230146057340afa72328cee0bcb06fcda27
Fariz <44774650+farizrachmansyah@users.noreply.github.com>
2019-03-13 11:42:18 +0700

    Add files via upload

---
+++ Tugas2.py
@@ -0,0 +1,5 @@
+x = int(input("Masukkan nilai x : "))
+y = int(input("Masukkan nilai y : "))
+h = x + y
+
+print(x, "+", y, "=", h)
---
+++ Tugas3.py
@@ -0,0 +1,5 @@
+a = int(input("Masukkan panjang alas : "))
+t = int(input("Masukkan tinggi : "))
+luas = float(0.5 * a * t)
+
+print("Luas segitiga =", luas)
